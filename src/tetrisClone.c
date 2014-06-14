a46f86cdfefcc16d33fda452ac3424ff1ea71a1c
Jacob Causon <jc29g12@soton.ac.uk>
2014-06-14 16:42:30 +0100

    tidy/SDL code moved to own file

--- src/tetrisClone.c
+++ src/tetrisClone.c
@@ -1,15 +1,12 @@
 // http://www.willusher.io/sdl2%20tutorials/2013/12/18/lesson-6-true-type-fonts-with-sdl_ttf/
 
-
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <time.h>
 #include <stdlib.h>
 #include "tetrisClone.h"
 
-#define SCORE_BUFFER_SIZE 13
+#define SCORE_BUFFER_SIZE 20
 
 const char* WINDOW_NAME = "Tetris clone";
 const int SCREEN_WIDTH = 640;
@@ -40,73 +37,6 @@ bool draw_required = true; //Has there been a change requiring a redraw?
 int fallTimer = 0;
 int fallTimerLimit = 35;//35; // 6 is quite playable -- 4 is near impossible
 int fallTimerMin = 4;
-
-int logSDLError(char* error)
-{
-	printf("%s: %s\n", error, SDL_GetError());
-    return 1;
-}
-
-SDL_Texture* loadTexture(const char* file, SDL_Renderer *ren)
-{
-	SDL_Texture *texture = IMG_LoadTexture(ren, file);
-	if (texture == NULL)		
-		logSDLError("LoadTexture");
-	return texture;
-}
-
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
-{
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	//Query the texture to get its width and height to use
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-
-
-void renderTextureClip(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip)
-{
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-    dst.w = clip->w;
-    dst.h = clip->h;
-	//Query the texture to get its width and height to use
-	//SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, clip, &dst);
-}
-
-SDL_Texture* renderText(char* message, TTF_Font* font,
-	SDL_Color color, SDL_Renderer *renderer)
-{
-	//Open the font
-	/*TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
-	if (font == nullptr){
-		logSDLError(std::cout, "TTF_OpenFont");
-		return nullptr;
-	}	*/
-
-	//We need to first render to a surface as that's what TTF_RenderText
-	//returns, then load that surface into a texture
-	SDL_Surface *surf = TTF_RenderText_Blended(font, message, color);
-	if (surf == NULL){
-		TTF_CloseFont(font);
-		logSDLError("TTF_RenderText");
-		return NULL;
-	}
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
-	if (texture == NULL){
-		logSDLError("CreateTexture");
-	}
-	//Clean up the surface and font
-	SDL_FreeSurface(surf);
-	//TTF_CloseFont(font);
-	return texture;
-}
 
 void drawPiece(SDL_Renderer* ren, PIECE *p, int xOrigin, int yOrigin, int color) 
 {
@@ -229,20 +159,9 @@ int main(int argc, char* argv[]) {
 
     SDL_Window *window;                    // Declare a pointer
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
-    //init image loader
-    if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG){
-	    logSDLError("IMG_Init");
-	    return 1;
+    if(init_SDL_all() != 0) {
+        return 1;
     }
-
-    if (TTF_Init() != 0){
-	    logSDLError("TTF_Init");
-	    return 1;
-    }
-
-
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
--- src/tetrisClone.h
+++ src/tetrisClone.h
@@ -1,5 +1,5 @@
 
-#include <SDL.h>
+#include "SDL_functions.h"
 #include "block_data.h"
 
 #define NUMBER_OF_BLOCKS 7
@@ -9,10 +9,7 @@
 #define DROP_START_X 3
 #define DROP_START_Y 1
 
-int logSDLError(char* error);
-SDL_Texture* loadTexture(const char* file, SDL_Renderer *ren);
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
-void renderTextureClip(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip);
+
 void drawPiece(SDL_Renderer* ren, PIECE *p, int xOrigin, int yOrigin, int color);
 int pickNewPiece();
 void deleteLine(int y);
